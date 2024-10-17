7748c756f5344b66380c3deb67207f614e9c448f
longnd04 <duyzfclong@gmail.com>
2024-10-17 17:52:10 +0700

    ui-component brand

---
+++ app/code/Aht/Demo/Model/ResourceModel/Brand/Grid/Collection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aht\Demo\Model\ResourceModel\Brand\Grid;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    protected $_idFieldName = 'entity_id';
+    protected $_eventPrefix = 'aht_demo_brand_collection';
+    protected $_eventObject = 'brand_collection';
+
+    /**
+     * Define the resource model & the model.
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Aht\Demo\Model\Brand', 'Aht\Demo\Model\ResourceModel\Brand');
+    }
+}
